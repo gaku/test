33af515a0a2a245502c5d96a853df292d0bde094
Gaku Ueda <gaku@gaku.net>
2011-06-04 19:36:01 -0700

    __FILE__ and File.dirname().

---
+++ file.rb
@@ -0,0 +1,9 @@
+# __FILE__ return the relative path for this source file.
+#   it is 'file.rb' in this example.
+# File.dirname() returns the relative directory.
+#   it is '.' for this example.
+#
+puts "__FILE__: #{__FILE__}"
+puts "File.dirname(__FILE__): #{File.dirname(__FILE__)}"
+
+
