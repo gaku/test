94f2b57908f6b04ba96676dedacd46c5420b53af
Gaku Ueda <gaku@gaku.net>
2011-06-05 22:12:45 -0700

    display interpretor availability

--- execjs_ex.rb
+++ execjs_ex.rb
@@ -5,3 +5,7 @@ require "execjs"
 # JavaScript array is converted into a ruby array.
 puts ExecJS.eval "'red yellow blue'.split(' ')"
 
+# I only had JavaScriptCore.
+puts "RubyRacer? #{ExecJS::Runtimes::RubyRacer.available?}"
+puts "JavaScriptCore? #{ExecJS::Runtimes::JavaScriptCore.available?}"
+
