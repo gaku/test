cf58958f84bd375ea2632f891d9fc8ba256f791c
Gaku Ueda <gaku@gaku.net>
2011-06-06 23:19:19 -0700

    Vector.unit_vector.

--- douglas_peucker/vector_test.rb
+++ douglas_peucker/vector_test.rb
@@ -17,4 +17,15 @@ class VectorTest < Test::Unit::TestCase
     assert_equal(0, v2.magnitude)
   end
 
+  def test_unit_vector
+    null_vector = DP::Vector.new(0, 0)
+    unit_vector = null_vector.unit_vector
+    assert_equal(0, unit_vector.magnitude)
+
+    v = DP::Vector.new(2, 2)
+    u2 = v.unit_vector
+    assert_equal(u2.x, u2.y)
+    assert_in_delta(1.0, u2.magnitude, 0.000001)
+  end
+
 end
