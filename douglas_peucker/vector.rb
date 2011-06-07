49d7b8f1cf507a5f2326e73761999d8d279708bd
Gaku Ueda <gaku@gaku.net>
2011-06-06 23:03:51 -0700

    Implemented Vector.magnitude.

--- douglas_peucker/vector.rb
+++ douglas_peucker/vector.rb
@@ -10,5 +10,16 @@ module DP
     def dot_product(v)
       return @x * v.x + @y * v.y
     end 
+
+    def magnitude
+      return Math.sqrt(@x * @x + @y * @y)
+    end
+
+    def unit_vector
+      if magnitude == 0
+        return Vector.new(0,0)
+      end
+      return Vector.new(@x / magnitude, @y / magnitude)
+    end
   end
 end
--- douglas_peucker/vector_test.rb
+++ douglas_peucker/vector_test.rb
@@ -6,6 +6,12 @@ class VectorTest < Test::Unit::TestCase
   def test_dot_product
     a = DP::Vector.new(1, 1)
     b = DP::Vector.new(2, -1)
-    assert_equal(1, a.dot_product(b))
+    assert_equal(1 * 2 + 1 * (-1), a.dot_product(b))
   end
+
+  def test_magnitude
+    v = DP::Vector.new(2, 3)
+    assert_equal(Math.sqrt(13), v.magnitude)
+  end
+
 end
