afc303d57b822894b34e9c12861480576cc3a0d0
Gaku Ueda <gaku@gaku.net>
2011-06-07 04:26:52 -0700

    Test two other cases for squared distance.

--- douglas_peucker/line_test.rb
+++ douglas_peucker/line_test.rb
@@ -14,5 +14,13 @@ class LineTest < Test::Unit::TestCase
     line = DP::Line.new(DP::Point.new(0,0), DP::Point.new(1, Math.sqrt(3)))
     point = DP::Point.new(0, Math.sqrt(3))
     assert_in_delta(0.75, line.distance_to_point_squared(point), 0.001)
+
+    # point2 is smaller than the line : (0,0) is the closest
+    point2 = DP::Point.new(-1, 0)
+    assert_in_delta(1, line.distance_to_point_squared(point2), 0.001)
+
+    # point3 is beyond the line : (1,sqrt(3)) is the closest
+    point3 = DP::Point.new(2, Math.sqrt(3))
+    assert_in_delta(1, line.distance_to_point_squared(point3), 0.001)
   end
 end
