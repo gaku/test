141e5841b2a1181847195d60f5dcec9cb989d436
Gaku Ueda <gaku@gaku.net>
2011-06-07 20:43:33 -0700

    Changed to use index rather than iterator.
    - debug code is still there

--- douglas_peucker/douglas_peucker.rb
+++ douglas_peucker/douglas_peucker.rb
@@ -10,17 +10,20 @@ module DP
     def simplify
       @output = {}
       # begining and ending points are always kept
-      @output[@original_points[0]] = true
-      @output[@original_points[@original_points.length - 1]] = true
+      @output[0] = true
+      @output[@original_points.length - 1] = true
 
-      douglas_peucker(0, @original_points.length - 1)
+      douglas_peucker(0, @original_points.length - 1, 0)
 
-      return @original_points.select do |elem|
-        @output[elem]
+      outputs = []
+      @output.keys.sort.each do |idx|
+        outputs << @original_points[idx]
       end
+      return outputs
     end
 
-    def douglas_peucker(start_vertex_index, end_vertex_index)
+    def douglas_peucker(start_vertex_index, end_vertex_index, depth)
+      puts "[#{depth}] douglas_peucker(#{start_vertex_index}, #{end_vertex_index})"
       return if end_vertex_index <= (start_vertex_index + 1) # nothing to do.
 
       # make line from start to end
@@ -28,23 +31,25 @@ module DP
 
       # find largest distance from intermediate points to this line
       max_dist_to_line_squared = 0
-      max_point = nil
+      max_point_index = nil
 
-      @original_points[start_vertex_index, end_vertex_index + 1].each do |point|      
+      (start_vertex_index..end_vertex_index).each do |index|
+        point = @original_points[index]
         dist_to_line_squared = line.distance_to_point_squared(point)
         if (dist_to_line_squared > max_dist_to_line_squared)
           max_dist_to_line_squared = dist_to_line_squared
-          max_point = point
+          max_point_index = index
         end
       end
 
       if max_dist_to_line_squared > @tolerance_squared
         # keep this point for output
-        @output[max_point] = true
+        @output[max_point_index] = true
         # split the polyline at the farthest vertex
-        index = @original_points.index(max_point)
-        douglas_peucker(start_vertex_index, index)
-        douglas_peucker(index, end_vertex_index)
+	puts "add #{max_point_index}"
+
+        douglas_peucker(start_vertex_index, max_point_index, depth+1)
+        douglas_peucker(max_point_index, end_vertex_index, depth+1)
       end
     end
   end
