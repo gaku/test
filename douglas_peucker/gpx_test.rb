7310d2f114bb5a54bb50c06afbb52d3303e94d7f
Gaku Ueda <gaku@gaku.net>
2011-06-07 23:03:59 -0700

    Handles GPX files.

---
+++ douglas_peucker/gpx_test.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'gpx'
+
+class GpxTest < Test::Unit::TestCase
+  def test_parse_file
+    gpx = GPX::GpxFile.new('data/sample.gpx')
+    gpx.parse_file
+  end
+end
