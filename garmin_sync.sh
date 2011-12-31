92756cd82cc58ae092e7b62bffc4b320fdb58561
Gaku Ueda <gaku@gaku.net>
2011-12-31 14:56:32 -0800

    Ejects Garmin GPS after successfully copying files. Handles a case when the Garmin GPS device is not mounted.

--- garmin_sync.sh
+++ garmin_sync.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
-SRC_DIR=/Volumes/GARMIN/Garmin/GPX
+VOLUME=GARMIN
+SRC_DIR=/Volumes/$VOLUME/Garmin/GPX
 SRC_ARCHIVE_DIR=$SRC_DIR/Archive
 DEST_DIR=/Users/gaku/safe/gps
+
+if [ ! -e $SRC_ARCHIVE_DIR ]; then
+  echo "ERROR: Garmin device is not mounted?"
+  exit 1
+fi
 
 for gpx in $SRC_ARCHIVE_DIR/*.gpx
 do
@@ -16,4 +22,5 @@ done
 SRC_CURRENT_DIR=$SRC_DIR/Current
 echo copy Current.gpx
 cp $SRC_CURRENT_DIR/Current.gpx $DEST_DIR
-
+# unmount the volume
+diskutil unmountdisk $VOLUME
